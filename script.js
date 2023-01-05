d0e2565ea83a2bc7d4171bb7a39dfd7fa8d4033b
Calum Read <calumreadmusic@gmail.com>
2023-01-05 12:37:37 +0700

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function greet(name) {
-    console.log(`hello ${name}`)
+    console.log(`hello ${name}`);
 }
 greet('Calum')
-console.log('greetings fuckface')
+console.log('greetings fuckface');
+
+const lineOne = ['Greetings Lord Fuckface,', '', '', '', ''];
+const lineTwo = ['You purple headed cocksplat,', 'Even flys think you smell bad,', '', '', ''];
+const lineThree = ['You make Trump look sane.', '', '', '', ''];
