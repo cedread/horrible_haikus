9702c4954d420e51e3998492a6d67f1e4d2b0486
Calum Read <calumreadmusic@gmail.com>
2023-01-10 12:03:29 +0700

    final commit for JS and reset.css

--- script.js
+++ script.js
@@ -5,25 +5,25 @@
 }*/
 const lineOne = ['Greetings Lord Fuckface,', 'Your face can scare ghosts,', 'I think you should know,', 'I fucking hate you,', 'You time wasting plank,'];
 const lineTwo = ['You purple headed cocksplat,', 'Even flys think you smell bad,', 'You\'re why we have birth control,', 'Please cease and fucking desist,', 'You\'ve the brains of a yoghurt,'];
-const lineThree = ['You make Trump look sane.', 'You prove Darwin wrong.', 'Go step on Legos.', 'Why even bother?', ''];
-
+const lineThree = ['You make Trump look sane.', 'You prove Darwin wrong.', 'Go step on Legos.', 'Why even bother?', 'No one can stand you.'];
 
 function haikuGenerate() {
     const returnLineOne = () => {
-        let num = Math.floor(Math.random() * lineOne.length - 1);
-        console.log(lineOne[num]);
+        let num = Math.floor(Math.random() * lineOne.length);
+        return lineOne[num];
     };
     const returnLineTwo = () => {
-        let num = Math.floor(Math.random() * lineTwo.length - 1);
-        console.log(lineTwo[num]);
+        let num = Math.floor(Math.random() * lineTwo.length);
+        return lineTwo[num];
     };
     const returnLineThree = () => {
-        let num = Math.floor(Math.random() * lineThree.length - 1);
-        console.log(lineThree[num]);
+        let num = Math.floor(Math.random() * lineThree.length);
+        return lineThree[num];
     };
-    returnLineOne();
-    returnLineTwo();
-    returnLineThree();
+    document.getElementById("lineOne").innerHTML = returnLineOne();
+    document.getElementById("lineTwo").innerHTML = returnLineTwo();
+    document.getElementById("lineThree").innerHTML = returnLineThree();
+    
 };
 
-haikuGenerate();
+/*haikuGenerate();*/
